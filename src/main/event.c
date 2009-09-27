1fef8c64bf31668808bb9c2a67c480d9d0a7f2d6
Alan T. DeKok <aland@freeradius.org>
2009-09-27 19:45:20 +0200

    Assign variable before using it

--- src/main/event.c
+++ src/main/event.c
@@ -1090,6 +1090,8 @@ static void no_response_to_proxied_request(void *ctx)
 #endif
 		check_for_zombie_home_server(request);
 
+	home = request->home_server;
+
 	/*
 	 *	The default as of 2.1.7 is to allow requests to
 	 *	fail-over to a backup home server when this one does
@@ -1106,8 +1108,6 @@ static void no_response_to_proxied_request(void *ctx)
 
 		post_proxy_fail_handler(request);
 	}
-
-	home = request->home_server;
 
 	/*
 	 *	Don't touch request due to race conditions
